cfd7a9200d2f58697a0cb4997fad9a1408cf3a57
nicolas <nicolas.souza@moinformatica.com>
2024-06-11 22:02:54 -0300

    Rest authentication

---
+++ src/main/java/dto/ServerSession.java
@@ -0,0 +1,15 @@
+package dto;
+
+public class ServerSession {
+
+    private static final ThreadLocal<Session> sessionThread = new ThreadLocal();
+
+    public static Session getSession() {
+        return sessionThread.get();
+    }
+
+    public static void setSession(Session session) {
+        sessionThread.set(session);
+    }
+
+}
